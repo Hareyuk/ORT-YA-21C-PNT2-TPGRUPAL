7c020555df196b3b4c49a40b20fac2c20e9aba11
Axel <55245056+Hareyuk@users.noreply.github.com>
2025-05-27 18:02:54 -0300

    Menú hamburgursa a mano
    Costó pero hecho

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
 import { Link, Slot } from 'expo-router';
 import Header from '../src/components/header';
 import Footer from '../src/components/footer';
-
+import { StyleSheet, View } from 'react-native';
 export default function Layout() {
   return (
     <>
-      <Header />
-      <Slot />
+    <View style={{flexDirection: 'column-reverse'}}>
       <Footer />
+        <Slot />
+      <Header />
+    </View>
     </>
   );
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,33 +1,92 @@
 import { Link, withLayoutContext } from "expo-router";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button, Platform } from "react-native";
+import Entypo from '@expo/vector-icons/Entypo';
+import { useState } from "react";
+import styled from "styled-components/native";
+
+const MenuNav = styled.View`
+        z-index: 15;
+        elevation: 15;
+        flex-direction: column;
+        padding-left: 2em;
+        padding-top: 3em;
+        position: fixed;
+        height: 100%;
+        left: 100%;
+        width: 350px;
+        transition: transform 0.3s ease-in-out;
+        transform: translateX(${({ posXMenu }) => posXMenu});
+        background-color: #201915;
+        `;
 
 export default function Header() {
-return (
-    <View style={styles.header}>
-        <View>
-          <Text style={styles.whiteText}>
-            <Link href="/" style={styles.marginLR}>Ir a Home</Link>
-            <Link href="/login" style={styles.marginLR}>Ir a Login</Link>
-            <Link href="/signup" style={styles.marginLR}>Ir a Registro</Link>
-            <Link href="/game" style={styles.marginLR}>Ir al Juego</Link>
-            <Link href="/lobby" style={styles.marginLR}>Ir al Lobby</Link>
-          </Text>
+
+    const posXActive = "-350px";
+    const posXInactive = "350px";
+
+    const [posXMenu, setPosXMenu] = useState(posXInactive);
+    const toggleMenu = () => { posXMenu === posXActive ? setPosXMenu(posXInactive) : setPosXMenu(posXActive) }
+    
+    return (
+        <View style={styles.header}>
+            <View style={styles.containerHeader}> 
+                <Text style={[styles.whiteText, styles.logoHeader]}>Logo</Text>
+                <Entypo style={styles.buttonOutsideHeader} name="menu" size={30} color="white" onPress={toggleMenu} />
+            </View>
+            <MenuNav posXMenu={posXMenu}>
+                <Entypo name="cross" size={30} color="white" onPress={toggleMenu} />
+                <Text style={[styles.whiteText, styles.menuNavBar]}>
+                    <Link href="/" style={styles.itemLink}>Home</Link>
+                    <Link href="/login" style={styles.itemLink}>Login</Link>
+                    <Link href="/signup" style={styles.itemLink}>Registro</Link>
+                    <Link href="/game" style={styles.itemLink}>Juego</Link>
+                    <Link href="/lobby" style={styles.itemLink}>Lobby</Link>
+                </Text>
+            </MenuNav>
         </View>
-    </View>
-);
-}
-const styles = StyleSheet.create({
-    header: {
-        backgroundColor: "#3a2a23",
-        padding: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: "#ddd",
-    },
-    whiteText: {
-        color: "#fff",
-    },
-    marginLR: {
-        marginLeft: 20,
-        marginRight: 20,
-    },
-});
+    );
+    }
+    const styles = StyleSheet.create({
+        header: {
+            width: "100%",
+            backgroundColor: "#3a2a23",
+        },
+        containerHeader:
+        {
+            display: "display",
+            position: "relative",
+            flexDirection: "row",
+            height: 50,
+            padding: 20,
+            borderBottomWidth: 1,
+            borderBottomColor: "#ddd",
+            maxWidth: 1920,
+            alignItems: "center",
+        },
+        buttonOutsideHeader: {
+            position: "absolute",
+            right: "1em",
+        },
+        logoHeader: {
+            fontSize: 24,
+            fontWeight: "bold",
+            color: "#fff",
+            position: "absolute",
+            left: "50%",
+            transform: [{ translateX: "-50%" }],
+        },
+        whiteText: {
+            color: "#fff",
+            fontSize: 18,
+        },
+        menuNavBar:
+        {
+            flexDirection: "column",
+            justifyContent: "space-between",
+            display: "flex"
+        },
+        itemLink: {
+            padding: 15,
+            textDecorationLine: "none",
+        },
+    });
